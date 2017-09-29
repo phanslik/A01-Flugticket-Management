45ff7ff9f19b796e802f1b8fe21e1afdbd76904d
phanslik-tgm <phanslik@users.noreply.github.com>
2017-09-29 10:16:43 +0200

    Add files via upload
    index.php leitet einfach nur weiter. ist da, damit es möglich ist später die sql server eingabe zu erleichtern.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Peters tolle Fluginfo</title>
+  </head>
+  <body>
+    <h1>Was wollen sie tun?</h1>
+      <a href="persanzeige.php">Personen anzeigen</a>
+      <br/>
+      Bitte ändern sie die das connection.php file!
+  </body>
+</html>
